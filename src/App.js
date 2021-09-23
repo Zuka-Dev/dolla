c0f328e235a822ad38ec8fb3c2ce62103850863a
Zuka-Dev <danazuka2003@gmail.com>
2021-09-23 17:20:59 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './pages'
+import Home from './pages/Home'
 import SigninPage from './pages/signin'
 function App() {
   
@@ -10,8 +10,8 @@ function App() {
     <Router>
       <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/signin" component={SigninPage} exact />
-      </Switch>
+          <Route path="/signin" component={SigninPage} exact /> 
+       </Switch>
     </Router>
   )
 }
